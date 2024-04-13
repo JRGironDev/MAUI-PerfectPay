9c34975fc1ee28e8aad7fa0e73fdda73a640f840
José Quiñónez <joserobertoq@outlook.com>
2024-04-13 03:55:06 -0600

    Add First Section

--- PerfectPay/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/PerfectPay.AssemblyInfo.cs
+++ PerfectPay/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/PerfectPay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerfectPay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+87fb99e9c1013e6d985cf85285a1462e3767cdc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerfectPay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerfectPay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
